a600005938f4e7a42757f5fc8f7859cbb1d15a8d
heyynat <natalifplima@gmail.com>
2022-11-05 16:54:28 -0400

    Feat: Task db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Coin.create!(
+  description: "Bitcoin",
+  acronym: "BTC",
+  url_image: "https://logos-world.net/wp-content/uploads/2020/08/Bitcoin-Emblem.png"
+)
+
+Coin.create!(
+  description: "Ethereum",
+  acronym: "ETC",
+  url_image: "https://download.logo.wine/logo/Ethereum/Ethereum-Logo.wine.png"
+)
+
+Coin.create!(
+  description: "Dash",
+  acronym: "DASH",
+  url_image: "https://cryptologos.cc/logos/dash-dash-logo.png"
+)
